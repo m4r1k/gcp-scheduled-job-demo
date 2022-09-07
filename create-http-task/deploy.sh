bc659091c4802d762aa6a84cb6ef686e997366e7
Federico Iezzi <federico.iezzi@gmail.com>
2022-09-07 14:06:17 +0000

    [create-http-task] use env variables

--- create-http-task/deploy.sh
+++ create-http-task/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# EDIT THESE!
-TASK_URL="https://task-handler-RANDOM_STRING.a.run.app" # Task handler endpoint; get after deploying Cloud Run
-
-TOPIC_ID="tasks-topic"
-QUEUE_ID="tasks-queue"
 FUNCTION_NAME="tasks-createHttpTask"
-REGION="europe-west1" # Needs to be a supported region
 ENTRY_POINT="createHttpTask"
-SA_NAME="sa-create-http-task" # Service account name
+
+REGION=europe-west1
 
 # Deploy
 gcloud functions deploy $FUNCTION_NAME \
@@ -17,5 +12,5 @@ gcloud functions deploy $FUNCTION_NAME \
   --timeout 10 \
   --memory 1Gi \
   --entry-point $ENTRY_POINT \
-  --service-account $SA_NAME@$PROJECT_ID.iam.gserviceaccount.com \
-  --set-env-vars PROJECT_ID=$PROJECT_ID,QUEUE_ID=$QUEUE_ID,REGION=$REGION,TASK_URL=$TASK_URL,SERVICE_ACCOUNT_NAME=$SA_NAME
+  --service-account $CREATEHTTPTASK_USERNAME@$PROJECT_ID.iam.gserviceaccount.com \
+  --set-env-vars PROJECT_ID=$PROJECT_ID,QUEUE_ID=$QUEUE_ID,REGION=$REGION,TASK_URL=$TASK_URL,SERVICE_ACCOUNT_NAME=$CREATEHTTPTASK_USERNAME
